1905001a64a76b0b5940f56d38e89cc1a286dbff
Craig Puxty <craig_puxty@talktalk.net>
2023-09-29 11:12:02 +0100

    Added "@babel/plugin-proposal-private-property-in-object":"7.21.11" to dependencies. Updated Greet.test with describe grouping.

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -1,14 +1,16 @@
 import { render, screen } from '@testing-library/react';
 import Greet from './Greet';
 
-test('Greet renders correctly.', () => {
-  render(<Greet />);
-  const textElement = screen.getByText('Hello');
-  expect(textElement).toBeInTheDocument();
-});
+describe('Greet', () => {
+  test('Renders correctly.', () => {
+    render(<Greet />);
+    const textElement = screen.getByText('Hello');
+    expect(textElement).toBeInTheDocument();
+  });
 
-test('Greet renders with a name.', () => {
-  render(<Greet name='Craig' />);
-  const textElement = screen.getByText('Hello Craig');
-  expect(textElement).toBeInTheDocument();
+  test('Renders with a name.', () => {
+    render(<Greet name='Craig' />);
+    const textElement = screen.getByText('Hello Craig');
+    expect(textElement).toBeInTheDocument();
+  });
 });
