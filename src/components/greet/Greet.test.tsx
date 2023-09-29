d62e888078814a16e7b182447068953dc199867c
Craig Puxty <craig_puxty@talktalk.net>
2023-09-29 11:24:49 +0100

    Removed alternate filename conventions.

--- src/components/greet/Greet.test.tsx
+++ src/components/greet/Greet.test.tsx
@@ -2,13 +2,13 @@ import { render, screen } from '@testing-library/react';
 import Greet from './Greet';
 
 describe('Greet', () => {
-  it('Renders correctly.', () => {
+  test('Renders correctly.', () => {
     render(<Greet />);
     const textElement = screen.getByText('Hello');
     expect(textElement).toBeInTheDocument();
   });
 
-  it('Renders with a name.', () => {
+  test('Renders with a name.', () => {
     render(<Greet name='Craig' />);
     const textElement = screen.getByText('Hello Craig');
     expect(textElement).toBeInTheDocument();
