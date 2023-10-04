dce0930d60740925508b666953fe3c3880292da7
Craig Puxty <craig_puxty@talktalk.net>
2023-10-04 15:32:26 +0100

    Disabled Application & Skills > App. Added counter > Counter. Added Counter > App.

--- src/App.tsx
+++ src/App.tsx
@@ -2,6 +2,7 @@ import './App.css';
 
 // Components
 import Application from './components/application/Application';
+import Counter from './components/counter/Counter';
 import Skills from './components/skills/Skills';
 
 // Data
@@ -10,8 +11,9 @@ import { skills } from './data/skills';
 function App() {
   return (
     <div className='App'>
-      <Application />
-      <Skills skills={skills} />
+      {/* <Application />
+      <Skills skills={skills} /> */}
+      <Counter />
     </div>
   );
 }
