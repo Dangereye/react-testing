9a37b6b3edeb972326b0e50deb3727777651c4a3
Craig Puxty <craig_puxty@talktalk.net>
2023-10-10 21:12:10 +0100

    Added providers/AppProviders.tsx.

---
+++ src/components/providers/AppProviders.tsx
@@ -0,0 +1,21 @@
+import { ThemeProvider, createTheme } from '@mui/material/styles';
+import CssBaseline from '@mui/material/CssBaseline';
+
+const theme = createTheme({
+  palette: {
+    mode: 'dark',
+  },
+});
+
+export default function AppProviders({
+  children,
+}: {
+  children: React.ReactNode;
+}) {
+  return (
+    <ThemeProvider theme={theme}>
+      <CssBaseline />
+      {children}
+    </ThemeProvider>
+  );
+}
