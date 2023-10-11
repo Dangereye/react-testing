6253e7d734f4c312bba02be13d5b31afd56731b8
Craig Puxty <craig_puxty@talktalk.net>
2023-10-11 16:25:11 +0100

    Updated CounterTwo.test to test function handlers are called - mocking functions.

--- src/components/counter_two/CounterTwo.test.tsx
+++ src/components/counter_two/CounterTwo.test.tsx
@@ -1,12 +1,32 @@
-import { render, screen } from '@testing-library/react';
+import { render, screen, act } from '@testing-library/react';
+import user from '@testing-library/user-event';
 
 // Components
 import CounterTwo from './CounterTwo';
 
 describe('Counter two', () => {
-  test('Renders correctly', () => {
+  test('Renders correctly.', () => {
     render(<CounterTwo count={0} />);
     const headingElement = screen.getByRole('heading', { name: 'Counter two' });
     expect(headingElement).toBeInTheDocument();
+  });
+
+  test('Function handlers are called.', async () => {
+    user.setup();
+    const incrementHandler = jest.fn();
+    const decrementHandler = jest.fn();
+    render(
+      <CounterTwo
+        count={0}
+        handleIncrement={incrementHandler}
+        handleDecrement={decrementHandler}
+      />
+    );
+    const incrementButton = screen.getByRole('button', { name: 'Increment' });
+    const decrementButton = screen.getByRole('button', { name: 'Decrement' });
+    await act(() => user.click(incrementButton));
+    await act(() => user.click(decrementButton));
+    expect(incrementHandler).toHaveBeenCalledTimes(1);
+    expect(decrementHandler).toHaveBeenCalledTimes(1);
   });
 });
